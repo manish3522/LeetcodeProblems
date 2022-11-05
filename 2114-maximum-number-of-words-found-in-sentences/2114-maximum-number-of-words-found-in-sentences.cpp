a75441cba893b0843bd27c0c06c77ae336d2040e
Manish Masiwal <35602576+manish3522@users.noreply.github.com>
2022-11-05 21:49:53 +0530

    Time: 38 ms (16.04%), Space: 9.8 MB (69.32%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -4,15 +4,32 @@ public:
         int n=s.size();
         int res=0;
         for(int i=0;i<n;i++) {
-            int space=0;
-            for(int j=0;j<s[i].size();j++) {
-                if(s[i][j]==' ') space++;
+            // int space=0;
+            // for(int j=0;j<s[i].size();j++) {
+            //     if(s[i][j]==' ') space++;
+            // }
+            // res=max(res,space+1);
+            int word=0;
+            for(int j=0;j<s[i].size();) {
+                if(s[i][j]==' ') {
+                    j++;
+                    continue;
+                }
+                int k=j;
+                word++;
+                while(k<s[i].size() && s[i][k]!=' ') {
+                    k++;
+                }
+                j=k;
             }
-            res=max(res,space+1);
+            res=max(res,word);
         }
         return res;
     }
 };
+
+//asd  fgh g
+
 // asdfggh
 // a s d
 
