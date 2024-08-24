f520c58b0f9b9a369a5f998a8bdd0a9454751216
Manish Masiwal <35602576+manish3522@users.noreply.github.com>
2024-08-24 17:22:53 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (70.26%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -4,12 +4,14 @@ public:
         int n=s.size(), open=0, close=0;
         for(int i=0; i<n; i++) {
             if(s[i]=='(' || s[i]=='*') open++;
-            else open--;
-
-            if(s[n-i-1]==')' || s[n-i-1]=='*') close++;
-            else close--;
-
-            if(open<0 || close<0) return false;
+            else close++;
+            if(open<close) return false;
+        }
+        open=0, close=0;
+        for(int i=n-1; i>=0; i--) {
+            if(s[i]==')' || s[i]=='*') close++;
+            else open++;
+            if(open>close) return false;
         }
         return true;
     }
